2bb42db2cd021382afeaeeb9726078a97656cdf7
mohamed naif <naifm371@gmail.com>
2024-07-21 08:38:15 +0300

    Add custom text field and custom bttn

--- lib/features/note_feature/presentation/views/widgets/add_note_bottom_sheet.dart
+++ lib/features/note_feature/presentation/views/widgets/add_note_bottom_sheet.dart
@@ -1,11 +1,33 @@
-
 import 'package:flutter/material.dart';
+
+import 'custom_bttn.dart';
+import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomTextField(hint: 'Title'),
+            SizedBox(
+              height: 18,
+            ),
+            CustomTextField(hint: 'Content', maxLines: 5),
+            SizedBox(
+              height: 40,
+            ),
+            CustomButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
